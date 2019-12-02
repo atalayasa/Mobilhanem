20b6c5e900581ffe385e08ad35e52c42e5844fbe
Atalay <atalay.asa@saglik.gov.tr>
2019-12-02 10:59:58 +0300

    Initial Commit

---
+++ PassingDataProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassingDataProject
+//
+//  Created by Atalay Asa on 2.12.2019.
+//  Copyright © 2019 Atalay Asa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
