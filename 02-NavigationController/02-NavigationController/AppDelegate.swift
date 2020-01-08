73208c3eb08b3549a7e3e6a61d86fd00f5bf5b09
Atalay <atalay.asa@saglik.gov.tr>
2020-01-08 16:47:48 +0300

    Navigation controller tutorial created

---
+++ 02-NavigationController/02-NavigationController/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  02-NavigationController
+//
+//  Created by Atalay Asa on 8.01.2020.
+//  Copyright © 2020 Atalay Asa. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ 02-NavigationController/02-NavigationController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  02-NavigationController
+//
+//  Created by Atalay Asa on 8.01.2020.
+//  Copyright © 2020 Atalay Asa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
