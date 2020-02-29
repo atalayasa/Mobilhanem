2e46f73d7ba3bdf71b8e5cdd10939bc747d7e897
atalayasa <atalayasa94@gmail.com>
2020-02-29 11:35:27 +0300

    Tabbar tutorial added

---
+++ 04-TabbarTutorial/04-TabbarTutorial/FirstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FirstViewController.swift
+//  04-TabbarTutorial
+//
+//  Created by Atalay Aşa on 24.02.2020.
+//  Copyright © 2020 Atalay Asa. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ 04-TabbarTutorial/04-TabbarTutorial/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  04-TabbarTutorial
+//
+//  Created by Atalay Aşa on 24.02.2020.
+//  Copyright © 2020 Atalay Asa. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
